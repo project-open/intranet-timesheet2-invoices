0040abaf3ac5c87c438315d914fe9e02a51934a6
Frank Bergmann <frank.bergmann@project-open.com>
2023-09-04 16:43:56 +0200

    cosine #5667: Changed order and sort order

--- tcl/intranet-timesheet2-invoices-procs.tcl
+++ tcl/intranet-timesheet2-invoices-procs.tcl
@@ -65,10 +65,10 @@ ad_proc im_timesheet_price_component { user_id company_id return_url} {
 <table border=0>
 <tr><td colspan=$colspan class=rowtitle align=center>[_ intranet-timesheet2-invoices.Price_List]</td></tr>
 <tr class=rowtitle> 
+	  <td class=rowtitle>[lang::message::lookup "" intranet-timesheet2-invoices.Project "Project"]</td>
 	  <td class=rowtitle>[_ intranet-timesheet2-invoices.UoM]</td>
 	  <td class=rowtitle>[_ intranet-timesheet2-invoices.Task_Type]</td>
 	  <td class=rowtitle>[_ intranet-timesheet2-invoices.Material]</td>
-	  <td class=rowtitle>[lang::message::lookup "" intranet-timesheet2-invoices.Project "Project"]</td>
 	  <td class=rowtitle>[lang::message::lookup "" intranet-timesheet2-invoices.From "From"]</td>
 	  <td class=rowtitle>[lang::message::lookup "" intranet-timesheet2-invoices.Through "Through"]</td>
 	  <td class=rowtitle>[_ intranet-timesheet2-invoices.Rate]</td>
@@ -93,10 +93,11 @@ ad_proc im_timesheet_price_component { user_id company_id return_url} {
 	where 
 		tp.company_id = :company_id
 	order by
-		tp.currency,
+		p.project_name,
 		tp.uom_id,
+		tp.task_type_id desc,
 		tp.material_id,
-		tp.task_type_id desc
+		tp.currency
     "
 
     set price_rows_html ""
@@ -112,13 +113,13 @@ ad_proc im_timesheet_price_component { user_id company_id return_url} {
 
 	append price_rows_html "
         <tr $bgcolor([expr {$ctr % 2}]) nobreak>
-	  <td><a href='$url'>$uom</a></td>
+	  <td>$project_name</td>
+	  <td>$uom</td>
 	  <td>$task_type</td>
 	  <td>$material</td>
-	  <td>$project_name</td>
 	  <td>$valid_from</td>
 	  <td>$valid_through</td>
-          <td>[format $price_format $price] $currency</td>
+          <td><a href='$url'>[format $price_format $price] $currency</a></td>
           <td><input type=checkbox name=price_id.$price_id id=price_id.$price_id></td>
 	</tr>"
 	incr ctr
